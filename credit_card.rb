26fb76416e794300e6472df2dadd75036dc871be
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-18 22:26:49 -0600

    Updated credit_card.rb
    * I added a payment method which takes a single argument (amount).
      It subtracts the amount from the balance owed.
    
    * I also added a balance return method, to call to output the
      balance, for testing purposes.
    
    [#132637001]

--- credit_card.rb
+++ credit_card.rb
@@ -1,12 +1,30 @@
+#!/usr/bin/env ruby
+
 # It's a class
 class CreditCard
+  attr_reader :balance
+
   def initialize
     @balance = 0.00
   end
 
-  attr_reader :balance
-
   def charge(amount)
     @balance += amount
   end
+
+  def payment(amount)
+    @balance -= amount
+  end
+
+  #keep track of running balance
+  def balance
+    puts @balance
+  end
 end
+
+cc = CreditCard.new
+
+cc.charge(2.50)
+cc.charge(9.33)
+cc.payment(5.00)
+cc.balance
